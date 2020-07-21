545f672d3f53432b65563a20cf88682f1bd78f9c
Evil Piggy <evilpiggyfoofoo@gmail.com>
2020-07-21 10:30:39 -0500

    'try-except'

---
+++ Math.py
@@ -0,0 +1,3 @@
+import math
+
+print(sum(map(lambda x: (1/1.02)**x, range(0,9999999))))
--- War_Pig.py
+++ War_Pig.py
@@ -214,18 +214,23 @@ async def clear_expired(ctx):
         for channel in category.channels:
             active_war = False
             topic = channel.topic
-            nation_link = topic.split()[2]
-            print(nation_link)
-            #Goes through a nation's wars and see if they still have wars with Carthago
-            for war in get_war_info(nation_link):
-                print(war)
-                if war['Aggressor Alliance'] == 'Carthago' or war['Defender Alliance'] == 'Carthago':
-                    active_war = True
-                    break
+            try:
+                nation_link = topic.split()[2]
+                #Goes through a nation's wars and see if they still have wars with Carthago
+                for war in get_war_info(nation_link):
+                    print(war)
+                    if war['Aggressor Alliance'] == 'Carthago' or war['Defender Alliance'] == 'Carthago':
+                        active_war = True
+                        break
 
-            if active_war == False:
-                await ctx.send(f'The war channel {channel.name} has been deleted.')
-                await channel.delete(reason="No existing Carthago wars with this nation")
+                if active_war == False:
+                    await ctx.send(f'The war channel {channel.name} has been deleted.')
+                    await channel.delete(reason="No existing Carthago wars with this nation")
+            except AttributeError:
+                await ctx.send(f"The war channel {channel.name} does not have a topic, thus we are unable to determine if war has been expired")
+
+            except: 
+                await ctx.send(f'Unexpected error has occured with {channel.name}, get piggy on it.')
     #Tells them if they don't have the permission to create the channel
     else:
         await ctx.send('You do not have permissions to create war channels')
@@ -234,110 +239,137 @@ async def clear_expired(ctx):
 
 @client.command()
 @commands.cooldown(1, 60, commands.BucketType.channel)
-async def war_info(ctx): #need to make this faster and more efficient
+async def war_info(ctx):
+    '''
+    Gets the all war information about the target nation
+
+    '''
     message = await ctx.send('Gathering information... please wait a few moments')
     book = load_workbook('spreadsheet/War.xlsx')
     sheet = book.active #active means last opened sheet
+    try:
+        nation_link = ctx.channel.topic.split()[2]
+        target_id = nation_link.split('=')[1]
+        sheet[f'B3'] = get_pnw_name(nation_link)
 
-    nation_link = ctx.channel.topic.split()[2]
-    target_id = nation_link.split('=')[1]
-    sheet[f'B3'] = get_pnw_name(nation_link)
+        #clears all old cells
+        for row in sheet['B3:S3']:
+          for cell in row:
+            cell.value = None
 
-    #clears all old cells
-    for row in sheet['B3:S3']:
-      for cell in row:
-        cell.value = None
+        for row in sheet['B6:S8']:
+          for cell in row:
+            cell.value = None
 
-    for row in sheet['B6:S8']:
-      for cell in row:
-        cell.value = None
+        for row in sheet['B10:S14']:
+          for cell in row:
+            cell.value = None
 
-    for row in sheet['B10:S14']:
-      for cell in row:
-        cell.value = None
+        #gets their basic military information and fills it in
+        nation_info = req_info(nation_link)
+        sheet[f'B3'] = nation_info['name']
+        sheet[f'C3'] = nation_info['alliance']
+        sheet[f'F3'] = nation_info['offensivewars']
+        sheet[f'H3'] = nation_info['nationid']
+        sheet[f'K3'] = nation_info['cities']
+        sheet[f'L3'] = nation_info['soldiers']
+        sheet[f'M3'] = nation_info['tanks']
+        sheet[f'N3'] = nation_info['aircraft']
+        sheet[f'O3'] = nation_info['ships']
 
+        #Function to fill in war information for offensive and defensive wars@
+        def war_fill(row, war, ID):
+            sheet[f'D{row}'] = war['turns_left']
 
-    nation_info = req_info(nation_link)
-    sheet[f'B3'] = nation_info['name']
-    sheet[f'C3'] = nation_info['alliance']
-    sheet[f'F3'] = nation_info['offensivewars']
-    sheet[f'H3'] = nation_info['nationid']
-    sheet[f'K3'] = nation_info['cities']
-    sheet[f'L3'] = nation_info['soldiers']
-    sheet[f'M3'] = nation_info['tanks']
-    sheet[f'N3'] = nation_info['aircraft']
-    sheet[f'O3'] = nation_info['ships']
+            #Basic control
+            if war['ground_control'] == ID:
+                sheet[f'P{row}'] = '✔'
+            elif war['ground_control'] == target_id:
+                sheet[f'P{row}'] = '✖'
 
+            if war['air_superiority'] == ID:
+                sheet[f'Q{row}'] = '✔'
+            elif war['air_superiority'] == target_id:
+                sheet[f'Q{row}'] = '✖'
 
-    def war_fill(row, war, ID):
-        sheet[f'D{row}'] = war['turns_left']
+            if war['blockade'] == ID:
+                sheet[f'R{row}'] = '✔'
+            elif war['blockade'] == target_id:
+                sheet[f'R{row}'] = '✖'
+            
+            req = req_info(f'https://politicsandwar.com/nation/id={ID}')
+            sheet[f'B{row}'] = req['leadername']
+            sheet[f'K{row}'] = req['cities']
+            sheet[f'E{row}'] = req['defensivewars']
+            sheet[f'F{row}'] = req['offensivewars']
+            sheet[f'L{row}'] = req["soldiers"]
+            sheet[f'M{row}'] = req["tanks"]
+            sheet[f'N{row}'] = req["aircraft"]
+            sheet[f'O{row}'] = req["ships"]
 
-        if war['ground_control'] == ID:
-            sheet[f'P{row}'] = '✔'
-        elif war['ground_control'] == target_id:
-            sheet[f'P{row}'] = '✖'
+        #Fils in offensive war unique attributes
+        off_wars = get_all_war_info(nation_link, True)
+        for index, war in enumerate(off_wars):
+            row = index + 10
+            sheet[f'C{row}'] = war['defender_alliance_name']
+            sheet[f'G{row}'] = war['defender_military_action_points']
+            sheet[f'H{row}'] = int(war['defender_resistance'])
+            sheet[f'I{row}'] = int(war['aggressor_resistance'])
+            sheet[f'J{row}'] = war['aggressor_military_action_points']
+            war_fill(row, war, war['defender_id'])
 
-        if war['air_superiority'] == ID:
-            sheet[f'Q{row}'] = '✔'
-        elif war['air_superiority'] == target_id:
-            sheet[f'Q{row}'] = '✖'
-
-        if war['blockade'] == ID:
-            sheet[f'R{row}'] = '✔'
-        elif war['blockade'] == target_id:
-            sheet[f'R{row}'] = '✖'
-        
-        req = req_info(f'https://politicsandwar.com/nation/id={ID}')
-        sheet[f'B{row}'] = req['leadername']
-        sheet[f'K{row}'] = req['cities']
-        sheet[f'E{row}'] = req['defensivewars']
-        sheet[f'F{row}'] = req['offensivewars']
-        sheet[f'L{row}'] = req["soldiers"]
-        sheet[f'M{row}'] = req["tanks"]
-        sheet[f'N{row}'] = req["aircraft"]
-        sheet[f'O{row}'] = req["ships"]
-
-    off_wars = get_all_war_info(nation_link, True)
-    for index, war in enumerate(off_wars):
-        row = index + 10
-        sheet[f'C{row}'] = war['defender_alliance_name']
-        sheet[f'G{row}'] = war['defender_military_action_points']
-        sheet[f'H{row}'] = int(war['defender_resistance'])
-        sheet[f'I{row}'] = int(war['aggressor_resistance'])
-        sheet[f'J{row}'] = war['aggressor_military_action_points']
-        war_fill(row, war, war['defender_id'])
-
-    def_wars = get_all_war_info(nation_link, False)
-    for index, war in enumerate(def_wars):
-        row = index + 6
-        sheet[f'C{row}'] = war['aggressor_alliance_name']
-        sheet[f'G{row}'] = war['aggressor_military_action_points']
-        sheet[f'H{row}'] = int(war['aggressor_resistance'])
-        sheet[f'I{row}'] = int(war['defender_resistance'])
-        sheet[f'J{row}'] = war['defender_military_action_points']
-        war_fill(row, war, war['aggressor_id'])
+        #Fills in defensive war unique attributes
+        def_wars = get_all_war_info(nation_link, False)
+        for index, war in enumerate(def_wars):
+            row = index + 6
+            sheet[f'C{row}'] = war['aggressor_alliance_name']
+            sheet[f'G{row}'] = war['aggressor_military_action_points']
+            sheet[f'H{row}'] = int(war['aggressor_resistance'])
+            sheet[f'I{row}'] = int(war['defender_resistance'])
+            sheet[f'J{row}'] = war['defender_military_action_points']
+            war_fill(row, war, war['aggressor_id'])
 
 
-    book.save('spreadsheet/War.xlsx')
+        book.save('spreadsheet/War.xlsx')
 
-    #excel2img.export_img("spreadsheet/War.xlsx","spreadsheet/image.png","War Scenario Sheet")
-    #image = discord.File("spreadsheet/image.png", filename="war_screen.png")
-    sheet = discord.File('spreadsheet/War.xlsx', filename="war_sheet.xlsx")
-    await message.delete()
-    await ctx.send(file = sheet)
+        #excel2img.export_img("spreadsheet/War.xlsx","spreadsheet/image.png","War Scenario Sheet")
+        #image = discord.File("spreadsheet/image.png", filename="war_screen.png")
+        sheet = discord.File('spreadsheet/War.xlsx', filename="war_sheet.xlsx")
+        await message.delete()
+        await ctx.send(file = sheet)
+    
+    #Except block
+    except AttributeError:
+        await ctx.send(f"The war channel {ctx.channel.name} does not have the correct topic format. We can not grab information for the war.")
+
+    except: 
+        await ctx.send(f'Unexpected error has occured with {ctx.channel.name}, get piggy on it.')
+
+
+
 
 
 @client.command()
-@commands.cooldown(1, 60, commands.BucketType.channel)
-async def graph(ctx, type, *alliances): #need to make this faster and more efficient
+@commands.cooldown(1, 60, commands.BucketType.user)
+async def graph(ctx, type, *alliances): 
+    '''
+    Graphs specific information for alliances
+    :param type: is the type of graph
+    :param alliances: is the list of alliances to graph
+
+    '''
     message = await ctx.send('Generating graph... please wait a few moments')
 
+    #graphs histogram 
     if type == 'hist':
         alliances_data = []
         label = []
+
+        #Makes it in the right format to be entered in url
         for name in alliances:
             label.append(name.replace('+', ' ').title())
 
+        #Runs through alliances and sees the number of nations
         for ally in alliances:
             await message.edit(content = f'Gathering information from {ally.replace("+", " ").title()}')
             print(ally)
@@ -345,12 +377,15 @@ async def graph(ctx, type, *alliances): #need to make this faster and more effic
             soup_data = BeautifulSoup(res.text, 'html.parser')
             data = soup_data.find(text = re.compile('Showing'))
             num_nations = float(data.split()[3])
+            
+            #Error handling for if the number of nations for the alliance is 0
             if num_nations == 0:
                 await ctx.send(f'Could not find any nations in the alliance {ally.replace("+", " ").title()}, make sure it is spelled correctly')
                 label.remove(ally)
                 continue
             alliance_city_data = []
 
+            #Grabs data for every nation in the alliance
             for nations in range(0, math.ceil(num_nations/3)):
                 res = requests.get(f'https://politicsandwar.com/index.php?id=15&keyword={ally}&cat=alliance&ob=score&od=DESC&maximum={50*(nations+1)}&minimum={50*nations}&search=Go&memberview=true')
                 soup_data = BeautifulSoup(res.text, 'html.parser')
@@ -388,23 +423,27 @@ async def graph(ctx, type, *alliances): #need to make this faster and more effic
         await message.delete()
         await ctx.send(file = pic)
 
-    if type == 'scat':
+    #Graphs for scatter plot with military informationS
+    elif type == 'scatter':
         milplot_array = np.array([['Alliance', 'Leader Name', 'Age', 'Cities', 'Soldiers Killed', 'Soldier Casualties'\
         , 'Tanks Killed', 'Tank Casualties', 'Planes Killed', 'Plane Casualties'\
         , 'Ships Killed', 'Ship Casualties', 'Infra Destroyed', 'Infra Lost', 'Money Looted']], dtype = object)
 
         html = []
+        #Gets it in the right format for html
         for alliance in alliances: 
             html.append(alliance.replace(' ', '+'))
 
         alliances_data = []
 
+        #Goes through each alliance and gets the nation links
         for ally in html:
             await message.edit(content = f'Gathering information from {ally.replace("+", " ").title()}')
             res = requests.get(f'https://politicsandwar.com/index.php?id=15&keyword={ally}&cat=alliance&ob=score&od=DESC&maximum=15&minimum=0&search=Go&memberview=true')
             soup_data = BeautifulSoup(res.text, 'html.parser')
             data = soup_data.find(text = re.compile('Showing'))
             num_nations = float(data.split()[3])
+            #Error handling if alliance has no nations
             if num_nations == 0:
                 await ctx.send(f'Could not find any nations in the alliance {ally.replace("+", " ").title()}, make sure it is spelled correctly')
                 label.remove(ally)
@@ -413,6 +452,7 @@ async def graph(ctx, type, *alliances): #need to make this faster and more effic
             alliance_city_data = []
             #alliance_city_data = defaultdict(lambda: 0, alliance_city_data)
 
+            #Goes to every nation in the alliance to grab military information
             for nations in range(0, math.ceil(num_nations/3)):
                 res = requests.get(f'https://politicsandwar.com/index.php?id=15&keyword={ally}&cat=alliance&ob=score&od=DESC&maximum={50*(nations+1)}&minimum={50*nations}&search=Go&memberview=true')
                 soup_data = BeautifulSoup(res.text, 'html.parser')
@@ -425,6 +465,7 @@ async def graph(ctx, type, *alliances): #need to make this faster and more effic
                 for nation_link in links:
                     mil = req_info(nation_link)
 
+                    #Creates dataframe
                     member_info = [mil['alliance'], mil['leadername'], int(mil['daysold']), mil['cities'],\
                         int(mil['soldierskilled']), int(mil['soldiercasualties']),int(mil['tankskilled']),int(mil['tankcasualties']),\
                         int(mil['aircraftkilled']),int(mil['aircraftcasualties']),int(mil['shipskilled']),int(mil['shipcasualties']),\
@@ -433,6 +474,8 @@ async def graph(ctx, type, *alliances): #need to make this faster and more effic
                     temp_arr = np.array([member_info], dtype = object)
 
                     milplot_array = np.append(milplot_array, temp_arr, axis=0)
+       
+
         await message.edit(content = f'Creating Graph')
         mil = pd.DataFrame(milplot_array[1:], range(len(milplot_array)-1), milplot_array[0])
         mil.to_excel('Excel_Sample.xlsx', sheet_name = 'Sheet1')
@@ -443,6 +486,7 @@ async def graph(ctx, type, *alliances): #need to make this faster and more effic
 
         await message.delete()
 
+        #Creates graph
         for image in stuff_to_graph:
             scattered0 = sns.lmplot(x = 'Age', y = image, data = df, hue = 'Alliance')
             ann(df, image)
@@ -450,6 +494,12 @@ async def graph(ctx, type, *alliances): #need to make this faster and more effic
 
             pic = discord.File(f"Scatter Plot Of {image}.png", filename=f"Scatter Plot Of {image}.png")
             await ctx.send(file = pic)
+   #If type is something weird
+    else:
+        await ctx.send(f'We are unable to identify graph type: {type}. The valid graph types are hist and scatter.')
+
+
+
 
 async def coord_perms(members, channel, channel_name, ctx):
     ''' 
@@ -495,6 +545,9 @@ async def find_members(ctx):
     member_ids = list(map(lambda member: discord.utils.get(ctx.guild.members, name = member).id if discord.utils.get(ctx.guild.members, name = member)!= None else None,member_dis))
     print(member_ids)'''
 
+
+
+
 def member_list(leader_name):
     '''
     Find the discord ID of the member given their leader name
@@ -522,15 +575,21 @@ def ping(ping_list):
 
 
 def ann(df, value):
-        slope, intercept, r_value, p_value, std_err = stats.linregress(df['Age'],df[value])
-        std_resid = math.sqrt((sum((df[value] - slope * df['Age'])**2))/(len(df.index)-2))
-        print(f'slope - {slope}, intercept - {intercept} std_resid - {std_resid}')
-        outliers = df[df[value] > (slope * df['Age'] + 0.8 * std_resid)]
-        plt.ylim(0, None)
-        plt.title(f'Scatter Plot of Nation Age Vs {value}')
-        for row in outliers.iterrows():
-            r = row[1]
-            plt.gca().annotate(f'{r["Leader Name"]}, {r["Cities"]}', xy=(r['Age'], r[value]), xytext=(2,2) , textcoords ="offset points", )
+    '''
+    Annotates the graph with the outliers
+    :param df: is the dataframe
+    :param value: Is the value to find outliers in
+
+    '''
+    slope, intercept, r_value, p_value, std_err = stats.linregress(df['Age'],df[value])
+    std_resid = math.sqrt((sum((df[value] - slope * df['Age'])**2))/(len(df.index)-2))
+    print(f'slope - {slope}, intercept - {intercept} std_resid - {std_resid}')
+    outliers = df[df[value] > (slope * df['Age'] + 0.8 * std_resid)]
+    plt.ylim(0, None)
+    plt.title(f'Scatter Plot of Nation Age Vs {value}')
+    for row in outliers.iterrows():
+        r = row[1]
+        plt.gca().annotate(f'{r["Leader Name"]}, {r["Cities"]}', xy=(r['Age'], r[value]), xytext=(2,2) , textcoords ="offset points", )
 
 
 
@@ -547,12 +606,17 @@ async def create_chan_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('Missing required argument, the correct format is:\
             !create_chan [nation_link] @member @member ... etc')
-    #If the command is on cooldown
 
+#If the command is on cooldown
 @war_info.error
 async def create_chan_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
         await ctx.send(f'The command is on cooldown for this channel, please try again in {error.retry_after:.3g} seconds')
+
+@graph.error
+async def create_chan_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(f'The command is on cooldown for you, please try again in {error.retry_after:.3g} seconds')
 
 
 
